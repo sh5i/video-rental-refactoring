6f4e136a68bad7b05fe9c03c62c1a1c3f1bae27c
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2022-01-27 13:24:18 +0900

    Refactor: Move Method (Rental#getFrequentRenterPoints, Movie)

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -21,11 +21,6 @@ public class Rental {
     }
 
     int getFrequentRenterPoints() {
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
