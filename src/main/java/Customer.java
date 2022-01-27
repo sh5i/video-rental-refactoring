35d3acc2e84ded25674e724878e7d4217663d41e
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2022-01-27 13:09:55 +0900

    Refactor: Extract Method (Customer#statement, getFrequentRenterPoints)

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -25,14 +25,7 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (final Rental each : rentals) {
-
-            // add frequent renter points
-            frequentRenterPoints++;
-
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            frequentRenterPoints += getFrequentRenterPoints(each);
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
@@ -44,5 +37,14 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
+    }
+
+    private int getFrequentRenterPoints(Rental each) {
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
     }
 }
