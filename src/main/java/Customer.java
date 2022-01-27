c65f0bab01d8f815c039357beeeb7d82a9f81df8
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2022-01-27 13:15:23 +0900

    Refactor: Inline Local Variable (Customer#statement, frequentRenterPoints)

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -19,20 +19,16 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
-
         String result = "Rental Record for " + getName() + "\n";
 
         for (final Rental each : rentals) {
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
         }
 
         // add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter points";
 
         return result;
     }
@@ -41,6 +37,14 @@ public class Customer {
         double result = 0;
         for (final Rental each : rentals) {
             result += each.getCharge();
+        }
+        return result;
+    }
+
+    int getFrequentRenterPoints() {
+        int result = 0;
+        for (final Rental each : rentals) {
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }
